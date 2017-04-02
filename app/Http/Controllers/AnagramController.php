e1de81810420aa776090b9ef7c7ecd37979932f4
David Kroll <curtain333-code@yahoo.com>
2017-04-02 15:22:59 -0400

    added AnagramController, modified web.php

---
+++ app/Http/Controllers/AnagramController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnagramController extends Controller
+{
+
+    /**
+    * GET
+    * /
+    */
+    public function index() {
+        return 'You are now in AnagramController.php';
+    }   
+
+
+
+} // end of AnagramController
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,20 @@
 <?php
 
-Route::get('/', function() {
-  return ('Welcome to Assignment 3');
-});
+    /**
+    * GET
+    * /
+    * use the index method of AnagramController:
+    */
+    Route::get('/', 'AnagramController@index');
+
+
+    /**
+    * GET
+    * /anything...
+    */
+    Route::get('/{urlFolder?}', function($urlFolder = '') {
+      return ('You typed ' . $urlFolder . ' after the domain name. Please remove this and use just the doman name to access Assignment 3. Thank you.');
+    });
 
 /*
 Route::get('/', function () {
@@ -10,10 +22,12 @@ Route::get('/', function () {
 });
 */
 
-/**
-* Log viewer
-* (only accessible locally)
-*/
-if(config('app.env') == 'local') {
-    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-}
+    /**
+    * Log viewer
+    * (only accessible locally)
+    */
+    if(config('app.env') == 'local') {
+        Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+    }
+
+// closing php tag intentionally omitted
