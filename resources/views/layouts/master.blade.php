26b1f38d31a39015b5b4e76ad244c7b2e90ffc80
David Kroll <curtain333-code@yahoo.com>
2017-04-02 18:23:02 -0400

    added stylesheet to public/css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,14 +5,16 @@
         <title>
             @yield('title', 'Anagrams')
         </title>
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">
     </head>
     <body>
+        <div class="container">
 
-    <p>This is content that is already in master.blade.php, not pulled in from elsewhere.</p>
+            <p>This is content that is already in master.blade.php, not pulled in from elsewhere.</p>
 
-    <section>
-		@yield('content')
-	</section>
-
+            <section>
+                @yield('content')
+	        </section>
+        </div>
     </body>
 </html>
